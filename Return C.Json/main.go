56c3ccdf732ab1dcaaaccb5410d71e4797708d62
Dessa <hafizhfakhri25@gmail.com>
2025-05-23 10:11:10 +0700

    Uji Postman

--- Return C.Json/main.go
+++ Return C.Json/main.go
@@ -18,5 +18,22 @@ func main() {
 		return c.JSON(user)
 	})
 
+	app.Post("/user", func(c *fiber.Ctx) error {
+		var user User
+
+		// Ambil body Json dan parsing ke struct
+		if err := c.BodyParser(&user); err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Data tidak Valid",
+			})
+		}
+
+		// Kembalikan data yang dikirim
+		return c.JSON(fiber.Map{
+			"message": "User diterima",
+			"user":    user,
+		})
+	})
+
 	app.Listen(":3000")
 }
